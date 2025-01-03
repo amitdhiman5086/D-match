b775218b79d1a5bdc38ca98d97ae61fbfeb177ca
Amit Dhiman <amitdhiman5086@gmail.com>
2025-01-03 11:32:38 +0530

    Build Feed Api And Get User Api

--- src/app.js
+++ src/app.js
@@ -3,14 +3,40 @@ const connectDb = require("./config/dataBase.js");
 const UserModel = require("./models/userSchema.js");
 const app = express();
 
+app.use(express.json());
+
+app.get("/user", async (req, res) => {
+  const emailId = req.body.email;
+  try {
+    const user = await UserModel.find({ email: emailId });
+    if (user.length === 0) {
+      res.status(404).send("User Not Found With This Email Id");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res
+      .status(400)
+      .send("Something went Wrong While Fetching By The Email........");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await UserModel.find();
+    const totalUser = allUsers.length;
+
+    res.json({
+      users: allUsers,
+      totalUser: totalUser,
+    });
+  } catch (error) {
+    res.status(400).send("Something Went Wrong in The Feed Api Section");
+  }
+});
+
 app.post("/signUp", async (req, res) => {
-  const userObject = {
-    firstName: "test",
-    lastName: "kumar",
-    password: "test@1232",
-    email: "test@gmail.com",
-    age: "12",
-  };
+  const userObject = req.body;
 
   try {
     const user = new UserModel(userObject);
